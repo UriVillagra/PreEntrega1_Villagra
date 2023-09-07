2bb2329e366a7b95ce7fc4ca457a6198c7b5f531
Urias Villagra <emavillagra00@gmail.com>
2023-09-06 23:48:33 -0300

    PreEntrega2Villagra

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import Reacts, { useState } from 'react';
 
 export default function App() {
+
+  const [textValue, setTextValue] = useState('')
+
+  const [itemslist, setitemslist] = useState([])
+
+  const onHandlechangeItem = (text) => setTextValue()
+
   return (
     <View style={styles.container}>
-      <Text>Hola Coder!!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.InputComtainer}>
+        <TextInput 
+            placeholder='Item de la Lista'
+            style={styles.Input}
+        />
+        <Button title='ADD' />
+      </View>
+      <View style={styles.listContainer}>
+        {itemslist.map(item => (
+          <View style={styles.TextContainer}>
+            <Text style={styles.Text}> item 1 </Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+  },
+  Input: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    width: 200,
+    fontSize: 24,
+  },
+  InputComtainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  listContainer:{
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "black",
+    borderWidth: 2,
+    marginTop: 20,
+    padding: 20,
+    backgroundColor: "blue",
+  },
+  Text:{
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "black",
+  },
+  TextContainer:{
+    borderColor: "black",
+    borderWidth: 2,
+    marginVertical: 10,
+    padding: 10,
+    width: "100%",
+    backgroundColor: "#f0f0f0",
+    alignItems: "center",
   },
 });
+
+
+
+
+{/* <View style={styles.TextContainer}>
+          <Text style={styles.Text}>
+            item 1
+          </Text>
+        </View>
+        <View style={styles.TextContainer}>
+          <Text style={styles.Text}>
+            item 2
+          </Text>
+        </View>
+        <View style={styles.TextContainer}>
+          <Text style={styles.Text}>
+            item 3
+          </Text>
+        </View> */}
